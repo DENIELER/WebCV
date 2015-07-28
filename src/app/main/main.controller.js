4e72527bbf8fe941012250537a3535ba871f4a17
unknown <danielostapenko@gmail.com>
2015-07-28 09:47:15 +0300

    Experiment with events

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,14 +10,47 @@ class MainCtrl {
     
     self.scrollManager = ScrollManagerService;
     
-    self.$scope.$on('meetAnimationFinished', function () {
-      self.$timeout(() => {
-        self.scrollManager.waitForScroll(() => {
-          console.info('startSkillsAnimation');
-          self.$scope.$broadcast('startSkillsAnimation');
-        });
-      }, 500);
-    });
+    self.waitEvent('meetAnimationFinished')
+    .waitScrollToElement('skills');
+//    .execEvent('startSkillsAnimation')
+//    .waitEvent('skillsAnimationFinished')
+//    .waitScrollToElement('beginning')
+//    .execEvent('startBeginningAnimation')
+//    .waitEvent('beginningAnimationFinished')
+//    .waitScrollToElement('firstSteps');
+    
+//    self.$scope.$on('meetAnimationFinished', function () {
+//      self.$timeout(() => {
+//        self.scrollManager.waitForScroll(() => {
+//          console.info('startSkillsAnimation');
+//          self.$scope.$broadcast('startSkillsAnimation');
+//        });
+//      }, 500);
+//    });
+  }
+  
+  waitEvent (eventName) {
+    var self = this;
+    
+    function waitScrollToElement (elementId) {
+      console.log('Hey! Handler ' + elementId);
+      
+      handler = function () {
+        console.log('Modified handler');
+      }
+    }
+    
+    function handler () {
+      console.log('Hey!');
+      
+      waitScrollToElement();
+    }
+    
+    self.$scope.$on(eventName, handler);
+    
+    return {
+      waitScrollToElement: waitScrollToElement
+    };
   }
 }
 
